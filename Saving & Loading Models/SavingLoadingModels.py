be0b75eb81d70dc6aa20f586c4bb48b084fd1666
Fatema Kotb <Fatema.Kotb02@eng-st.cu.edu.eg>
2024-05-13 20:00:52 +0300

    Fixed imports/exports

---
+++ Saving & Loading Models/SavingLoadingModels.py
@@ -0,0 +1,19 @@
+from joblib import dump
+
+def save_Kmeans_model(kmeans):
+    # Save the KMeans model
+    dump(kmeans, 'kmeans_model.joblib')
+
+def save_SVM_model(svm):
+    # Save the SVM model
+    dump(svm, 'svm_model.joblib')
+
+from joblib import load
+
+def load_Kmeans_model():
+    # Load the KMeans model
+    return load('kmeans_model.joblib')
+
+def load_SVM_model():
+    # Load the SVM model
+    return load('svm_model.joblib')
